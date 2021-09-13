3caf3b15cf0ab8b657d04e0ef1f0f6ce5c74bf65
Michael Whittle <whittlem@users.noreply.github.com>
2021-09-13 08:23:37 +0100

    Create Killable.sol

---
+++ contracts/Killable.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+import './Pausable.sol';
+
+contract Killable is Pausable {
+
+    event LogKill(address sender, address recipient, uint balance);
+
+    constructor(bool initialPausedState) 
+        payable 
+        Pausable(initialPausedState)
+    { }
+
+    function killContract() 
+        isOwner 
+        public 
+    {
+        emit LogKill(msg.sender, msg.sender, address(this).balance);
+        address payable addr = payable(msg.sender);
+        selfdestruct(addr);
+    }
+
+    function killContractAndTransfer(address recipient) 
+        isOwner 
+        public 
+    {
+        //require(recipient > 0);
+        emit LogKill(msg.sender, recipient, address(this).balance);
+        address payable addr = payable(recipient);
+        selfdestruct(addr);
+    }
+
+}
