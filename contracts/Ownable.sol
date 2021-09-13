4748973a75a5c578162a1c22a548d101e1a70548
Michael Whittle <whittlem@users.noreply.github.com>
2021-09-13 08:22:45 +0100

    Create Ownable.sol

---
+++ contracts/Ownable.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+contract Ownable {
+    address private owner;
+    
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    
+    modifier isOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        emit OwnerSet(address(0), owner);
+    }
+
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
